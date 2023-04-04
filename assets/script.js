3997fafb00b522c8485f6b4ed8cc7bb83387dccc
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-04-04 18:30:08 -0400

    Added basic bones of generator. WIP

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,49 @@
 // Assignment code here
-var lowCase = "abcdefghijklmnopqrstuvwxyz"
+var lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var Num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var Symbols = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+var minLength = 8;
+var maxLength = 128;
+var protoPass = [];
+var randomPassword = [];
+function generatePassword() {
+  var lengthConfirm = prompt("Choose a length for your password \nMust be between 8 and 128 numbers!");
+  if (lengthConfirm < minLength || lengthConfirm > maxLength) {
+    alert("Password must be between 8 and 128 characters!");
+    return;
+  } else {
+    alert("Great!");
+    var upperCase = confirm("Do you want to use Uppercase Letters?")
+    var lowerCase = confirm("Do you want to use Lowercase Letters?")
+    var numbers = confirm("Do you want to use numbers?")
+    var specChara = confirm("Do you want to use Special Symbols?")
+    for (i = 0; i < lengthConfirm; i++) {
+      if (upperCase) {
+        protoPass.push(upCase);
+      }
+      if (lowerCase) {
+        protoPass.push(lowCase);
+      }
+      if (numbers) {
+        protoPass.push(Num);
+      }
+      if (specChara) {
+        protoPass.push(Symbols);
+      }
+    }
+    for (i = 0; i < lengthConfirm; i++) {
+      var random = math.floor(math.random() * protoPass.length);
+      var randomArray = protoPass[random];
+      randomPassword.push(randomArray);
+      return randomPassword;
+    }
+    randomPassword = randomPassword.join('');
+    return randomPassword;
+  }
+  
+}
 
-var upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var symbols ="!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -19,3 +58,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
